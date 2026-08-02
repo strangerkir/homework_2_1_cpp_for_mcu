60f54846ea6b434140d41d64f1a9e0515b65b69e
Kyrylo Braslavskyi <strangerkir@gmail.com>
2026-08-02 14:31:22 +0300

    Fix code style: remove extra space

--- include/config.h
+++ include/config.h
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 class Config {
-    public :
+    public:
         static constexpr unsigned long serial_baud = 115200;
         static constexpr uint8_t led_pin = 16;
         static constexpr unsigned long blink_interval = 1000; // Blink interval in ms, unsigned long for millis() compatibility
